c98e876ea899d93de11dda1278099c7eda63e026
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-25 02:34:32 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,2 +1,10 @@
-numbers = [1,2,3]
-p square_array(numbers)
+def square_array(array)
+  numbers = []
+  index = 0
+  while array[index] do 
+    numbers.push(array[index]*array[index])
+    index +=1
+  end
+  numbers
+end
+p square_array(nil)
